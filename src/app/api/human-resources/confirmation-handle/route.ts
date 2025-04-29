fd25cdcf6172b15df0472bb4e035c0d34f9e3bb1
Roger Moreno <roger.moreno@ui.ac.id>
2025-04-29 21:16:30 +0700

    [feat/hr-management 48777d1] feat:[PBI-4] PUT Assign Employee to an Event & Jobdesk

--- src/app/api/human-resources/confirmation-handle/route.ts
+++ src/app/api/human-resources/confirmation-handle/route.ts
@@ -1,48 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/utils/prisma";
 
-export async function GET() {
-  try {
-    const users = await prisma.user.findMany({
-      include: {
-        userEvents: true,
-      },
-    });
-
-    type UserWithUserEvents = {
-      id: string;
-      is_active: boolean;
-      userEvents: any[];
-    };
-
-    const updatedUsers = users.map((user: UserWithUserEvents) => {
-      let status: string;
-
-      if (!user.is_active) {
-        status = "inactive";
-      } else if (user.userEvents.length === 0) {
-        status = "unassigned";
-      } else {
-        status = "assigned";
-      }
-
-      return {
-        ...user,
-        status,
-      };
-    });
-
-    console.log("Updated Users:", updatedUsers);
-
-    return NextResponse.json(updatedUsers);
-  } catch (error: unknown) {
-    console.error("Error fetching users:", error);
-    return NextResponse.json({
-      error: "Failed to fetch users",
-    });
-  }
-}
-
 export async function POST(req: Request) {
   try {
     const data = await req.json();
@@ -58,7 +16,7 @@ export async function POST(req: Request) {
     const existingAssignments = await prisma.userEvent.findMany({
       where: {
         userId: user_id,
-        eventId: { in: event_ids },  // Check if the user is assigned to any of the selected events
+        eventId: { in: event_ids },
       },
     });
 
@@ -69,20 +27,6 @@ export async function POST(req: Request) {
           details: "User is already assigned to this event.",
         },
         { status: 409 }
-      );
-    }
-
-    const userAssignments = await prisma.userEvent.findMany({
-      where: { userId: user_id },
-    });
-
-    if (userAssignments.length > 0) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: "User is already assigned to other events. Do you want to proceed with the new event?",
-        },
-        { status: 200 }
       );
     }
 
